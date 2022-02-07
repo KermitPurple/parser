8f0fc81f10a1d2c25615fe9b6c531c34ea441323
Shane McDonough <shanemcdman@gmail.com>
2022-02-07 14:08:19 -0800

    Create and test eval method

--- src/parser/tree.rs
+++ src/parser/tree.rs
@@ -7,6 +7,20 @@ pub enum Tree {
     Rem(Box<Tree>, Box<Tree>),
 }
 
+impl Tree {
+    fn eval(&self) -> i64 {
+        use Tree::*;
+        match self {
+            Int(num) => *num,
+            Add(a, b) => a.eval() + b.eval(),
+            Sub(a, b) => a.eval() - b.eval(),
+            Mul(a, b) => a.eval() * b.eval(),
+            Div(a, b) => a.eval() / b.eval(),
+            Rem(a, b) => a.eval() % b.eval(),
+        }
+    }
+}
+
 impl ToString for Tree {
     fn to_string(&self) -> String {
         use Tree::*;
@@ -24,6 +38,21 @@ impl ToString for Tree {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_eval(){
+        assert_eq!(3, Tree::Add(Box::new(Tree::Int(1)), Box::new(Tree::Int(2))).eval());
+        assert_eq!(
+            7,
+            Tree::Add(
+                Box::new(Tree::Int(1)),
+                Box::new(Tree::Mul(
+                    Box::new(Tree::Int(2)),
+                    Box::new(Tree::Int(3))
+                ))
+            ).eval(),
+        );
+    }
 
     #[test]
     fn test_to_string(){
